ca060331d14c24f7d780a783b4842a4db2aa912c
SzymonGit2115 <104094742+SzymonGit2115@users.noreply.github.com>
2026-03-15 15:41:42 +0100

    dodanie tabela HealthMetrics

---
+++ src/main/java/pl/wsb/fitnesstracker/healthmetrics/HealthMetrics.java
@@ -0,0 +1,55 @@
+package pl.wsb.fitnesstracker.healthmetrics;
+
+
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import pl.wsb.fitnesstracker.user.api.User;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "health_metrics")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
+public class HealthMetrics {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Nullable
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "date", nullable = false)
+    private LocalDate date;
+
+    @Column(name = "weight")
+    private Double weight;
+
+    @Column(name = "height")
+    private Double height;
+
+    @Column(name = "heart_rate")
+    private Integer heartRate;
+
+    public HealthMetrics(
+            final User user,
+            final LocalDate date,
+            final Double weight,
+            final Double height,
+            final Integer heartRate) {
+
+        this.user = user;
+        this.date = date;
+        this.weight = weight;
+        this.height = height;
+        this.heartRate = heartRate;
+    }
+}
