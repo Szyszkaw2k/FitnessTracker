62bb93d6d5882b086661f8a72820cb816e725829
SzymonGit2115 <104094742+SzymonGit2115@users.noreply.github.com>
2026-03-29 15:55:26 +0200

    dodanie testów pkt 11 cz 2

--- src/test/java/pl/wsb/fitnesstracker/DatabaseSchemaTest.java
+++ src/test/java/pl/wsb/fitnesstracker/DatabaseSchemaTest.java
@@ -68,7 +68,7 @@ class DatabaseSchemaTest {
     void usersTableHasExpectedColumns() throws Exception {
         try (Connection conn = dataSource.getConnection()) {
             Set<String> cols = tableColumns(conn, "users");
-            assertThat(cols).contains("id", "email");
+            assertThat(cols).contains("id", "email", "first_name", "last_name");
         }
     }
 
@@ -76,7 +76,7 @@ class DatabaseSchemaTest {
     void healthMetricsTableHasExpectedColumns() throws Exception {
         try (Connection conn = dataSource.getConnection()) {
             Set<String> cols = tableColumns(conn, "health_metrics");
-            assertThat(cols).contains("id", "user_id");
+            assertThat(cols).contains("id", "user_id", "date", "weight", "height");
         }
     }
 
@@ -84,7 +84,7 @@ class DatabaseSchemaTest {
     void statisticsTableHasExpectedColumns() throws Exception {
         try (Connection conn = dataSource.getConnection()) {
             Set<String> cols = tableColumns(conn, "statistics");
-            assertThat(cols).contains("id", "user_id");
+            assertThat(cols).contains("id", "user_id", "total_trainings", "total_distance");
         }
     }
 
